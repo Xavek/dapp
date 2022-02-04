9453c90860e25eac7f81c6d6ed7cfaea5d712628
gautam2002 <theofficalgautam200@gmail.com>
2022-02-04 18:12:57 +0545

    added deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-waffle");
+
+// here configure the private_keys and alchemy_keys to deploy the contracts.
 module.exports = {
   solidity: "0.8.9",
 };
