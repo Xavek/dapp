4d901046d7eda4ea272dc5b539bca827e8e1ff28
gautam2002 <theofficalgautam200@gmail.com>
2022-02-04 17:57:03 +0545

    added hadhat/console.log

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
+// With this we can write Javascript code inside solidity file
+// Mainly used for used for debugging the Smart contract.
+// we can combine the JS and Sol code also
+// import "hardhat/console.sol";
+// Smaple usage of this feature is at line 24.
+// The logs are logged during the npx hardhat test.
+
 contract Token {
     string public name = "Hardhat Token";
     string public symbol = "HTT";
@@ -15,6 +22,7 @@ contract Token {
     }
 
     function transfer(address to, uint256 amount) external {
+        // console.log("This amount is send from %s amount %s", to, amount);
         require(balances[msg.sender] >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
